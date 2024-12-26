017b44bbe22f0a037e8d1f94595f2fbefe7ce3de
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-26 13:04:39 +0100

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import FormWithout from './components/FormWithout'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+    <FormWithout />
     </>
   )
 }
